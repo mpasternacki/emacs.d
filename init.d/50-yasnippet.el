273825f0466ae7e1e6c6b8937a558eff81b322f1
Maciej Pasternacki <maciej@pasternacki.net>
2012-11-18 02:30:06 +0100

    Update yasnippet config

--- init.d/50-yasnippet.el
+++ init.d/50-yasnippet.el
@@ -1,9 +1,12 @@
 (require 'yasnippet)
 
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/lisp/yasnippet/snippets")
-(yas/load-directory "~/.emacs.d/snippets")
-(yas/load-directory "~/.emacs.d/lisp/cucumber.el/snippets")
+(setq yas/snippet-dirs '("~/.emacs.d/lisp/yasnippet/snippets"
+			 "~/.emacs.d/snippets"
+			 "~/.emacs.d/extras/imported"
+			 "~/.emacs.c/lisp/cucumber.el/snippets"))
+
+(yas/global-mode 1)
+
 ;; (yas/set-mode-parent 'fortran-mode 'f90-mode)
 ;; (yas/set-mode-parent 'nxhtml-mode 'nxml-mode)
 ;; (yas/set-mode-parent 'javascript-mode 'cc-mode)
