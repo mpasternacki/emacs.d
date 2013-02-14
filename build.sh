df70ad5ab3aef3911bdaa2427063c19ab549c424
Maciej Pasternacki <maciej@pasternacki.net>
2013-02-15 00:34:28 +0100

    drop evoker

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e -x
+
+rm -rf site-lisp
+cp -R vendor site-lisp
+
+cd site-lisp
+/usr/local/bin/emacs -batch -f batch-file-compile *.el || :
+
+mkdir _src
+mv auctex _src
+cd _src/auctex
+./configure --prefix=`pwd` \
+    --with-texmf-dir=$HOME/Library/texmf \
+    --with-lispdir=$HOME/.emacs.d/site-lisp
+make
+make install
+
+cd ../../gnuplot-mode
+./configure
+make
