e00b85fe6ff8709357d5ada20eb3d048d66e7237
Maciej Pasternacki <maciej@pasternacki.net>
2013-02-15 01:21:43 +0100

    typo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf site-lisp
 cp -R vendor site-lisp
 
 cd site-lisp
-/usr/local/bin/emacs -batch -f batch-file-compile *.el || :
+/usr/local/bin/emacs -batch -f batch-byte-compile *.el || :
 
 mkdir _src
 mv auctex _src
