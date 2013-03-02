2cfc8f4d2dd2f7720305fcbf0142a34c3a68b672
Maciej Pasternacki <maciej@pasternacki.net>
2013-03-02 12:06:47 +0100

    Use local ruby-mode.

--- init.d/00-load-path.el
+++ init.d/00-load-path.el
@@ -1,22 +1,18 @@
 ;;; Add subdirs of ~/.emacs.d/site-lisp/ to load-path.
 (setq load-path
-      (nconc (list
-              (car (sort (file-expand-wildcards "~/.rvm/src/ruby-*/misc")
-                         '(lambda (a b)
-                            (string< b a))))
-              "~/Projekty/chef-mode"
-              )
-             (mapcar (lambda (dir)
-                       (concat "~/.emacs.d/site-lisp/" dir))
-                     '(""
-                       "color-theme"
-                       "gnuplot-mode"
-                       "org-mode/lisp"
-                       "ess/lisp"
-                       "yasnippet"
-                       "clojure-mode"
-                       "rvm.el"
-                       "magit"
-                       "coffee-mode"
-                       "helm"))
+      (nconc (list "~/Projekty/chef-mode")
+             (mapcar
+              (lambda (dir) (concat "~/.emacs.d/site-lisp/" dir))
+              '(""
+                "color-theme"
+                "gnuplot-mode"
+                "org-mode/lisp"
+                "ess/lisp"
+                "yasnippet"
+                "clojure-mode"
+                "rvm.el"
+                "magit"
+                "coffee-mode"
+                "helm"
+                "ruby-mode"))
              load-path))
