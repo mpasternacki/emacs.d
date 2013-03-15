ceb3aa890205944ca83788aa4054769ccceb6dee
Maciej Pasternacki <maciej@pasternacki.net>
2013-03-15 19:32:59 +0000

    hack ruby indentation

--- init.d/50-ruby.el
+++ init.d/50-ruby.el
@@ -4,3 +4,30 @@
 
 (require 'chef-mode)
 (global-chef-mode 1)
+
+(defadvice ruby-indent-line (after line-up-args activate)
+  (let (indent prev-indent arg-indent)
+    (save-excursion
+      (back-to-indentation)
+      (when (zerop (car (syntax-ppss)))
+        (setq indent (current-column))
+        (skip-chars-backward " \t\n")
+        (cond
+         ((eq ?\\ (char-before))
+          (back-to-indentation)
+          (setq prev-indent (current-column))
+          (setq arg-indent (+ ruby-indent-level prev-indent)))
+         ((eq ?, (char-before))
+          (ruby-backward-sexp)
+          (back-to-indentation)
+          (setq prev-indent (current-column))
+          (skip-syntax-forward "w_.")
+          (skip-chars-forward " ")
+          (setq arg-indent (current-column))))))
+    (when prev-indent
+      (let ((offset (- (current-column) indent)))
+        (cond ((< indent prev-indent)
+               (indent-line-to prev-indent))
+              ((= indent prev-indent)
+               (indent-line-to arg-indent)))
+        (when (> offset 0) (forward-char offset))))))
